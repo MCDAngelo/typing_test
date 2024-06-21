628f0f0b35bf9416e461f7cebb94d2eddcbcdf49
MCDAngelo <dangelo.maria@gmail.com>
2024-06-21 16:42:03 -0600

    Move timer handling to test typer class, highlight current word, check spelling of input

--- typer.py
+++ typer.py
@@ -3,69 +3,178 @@ from tkinter import messagebox
 
 import ttkbootstrap as ttk
 
+from paragraphs import generate_random_paragraph
+from timer import Timekeeper
+
 
 class TypeTest:
-    def __init__(self, root, timer):
+    def __init__(self, root):
         self.window = root
+        self.test_duration = 6
+        self.timekeeper = Timekeeper(self.window)
         self.create_original_textbox()
         self.create_input_text()
-        self.test_on = False
         self.window.bind("<Key>", self.key_handler)
-        self.reset_vars(timer)
+        self.reset_vars()
 
-    def reset_vars(self, timer):
-        self.timer = timer
+    def reset_vars(self):
+        self.timekeeper.new_timer(self.test_duration)
         self.key_strokes = []
         self.final_input_words = []
         self.text_to_copy = generate_random_paragraph()
         self.current_word_index = 0
+        self.current_char_index = 0
         self.current_word = ""
-        self.current_input = ""
+        self.highlighted_word = ""
+        self.highlighted_start = "1.0"
+        self.highlighted_end = "1.0"
+        self.current_word_spellcheck_status = None
+        self.spell_check_start = "1.0"
+        self.spell_check_end = "1.0"
+        self.update_original_textbox()
+        self.input_text.config(state=tk.NORMAL)
+        self.input_text.delete(0, tk.END)
+
+    def start_test(self):
+        self.timekeeper.timer.start()
+        self.window.after(1000, self.timekeeper.tick)
+        self.run_test()
+
+    def run_test(self):
+        if self.timekeeper.timer.running:
+            self.window.after(1000, self.run_test)
+        else:
+            self.input_text.config(state="disabled")
+            self.spellcheck_current_input()
+            self.get_word_from_input()
+            self.check_typing()
+
+    def create_input_text(self):
+        self.input_text = ttk.Entry(self.window, width=30)
+        self.input_text.grid(row=3, column=1, columnspan=2, padx=50, pady=(0, 20))
 
     def create_original_textbox(self):
         self.original_textbox = ttk.Text(self.window, width=80, height=10)
         self.original_textbox.insert(tk.END, "Example text")
         self.original_textbox.configure(state=tk.DISABLED)
         self.original_textbox.grid(row=1, column=1, columnspan=3, padx=20, pady=10)
+        self.original_textbox.tag_configure("highlight", background="skyblue")
+        self.original_textbox.tag_configure("correct_word", foreground="blue")
+        self.original_textbox.tag_configure("correct_input", foreground="white")
+        self.original_textbox.tag_configure("incorrect_input", foreground="red")
 
-    def update_original_textbox(self, text):
-        self.text_to_copy = text.split(" ")
+    def update_original_textbox(self):
+        self.words_to_copy = self.text_to_copy.split(" ")
         self.original_textbox.configure(state=tk.NORMAL)
         self.original_textbox.delete("1.0", tk.END)
-        self.original_textbox.insert(tk.END, text)
+        self.original_textbox.insert(tk.END, self.text_to_copy)
         self.original_textbox.configure(state=tk.DISABLED)
-
-    def create_input_text(self):
-        self.input_text = ttk.Entry(self.window, width=30)
-        self.input_text.grid(row=3, column=1, columnspan=2, padx=50, pady=(0, 10))
+        self.current_word = self.words_to_copy[self.current_word_index]
+        self.highlight_current_word()
 
     def get_word_from_input(self):
         word = self.input_text.get()
         self.final_input_words.append(word)
         self.input_text.delete(0, tk.END)
 
+    def highlight_current_word(self):
+        if self.highlighted_word != self.current_word:
+            self.original_textbox.tag_remove(
+                "highlight", self.highlighted_start, self.highlighted_end
+            )
+            self.current_end_index = self.current_char_index + len(self.current_word)
+            start = f"1.{self.current_char_index}"
+            end = f"1.{self.current_end_index}"
+            self.original_textbox.tag_add("highlight", start, end)
+            self.highlighted_word = self.current_word
+            self.highlighted_start = start
+            self.highlighted_end = end
+
+    def spellcheck_current_input(self):
+        current_input_char_index = len(self.input_text.get())
+        partial_original = self.current_word[:current_input_char_index]
+        current_input_end_index = min(
+            (self.current_char_index + current_input_char_index),
+            self.current_end_index,
+        )
+        start = f"1.{self.current_char_index}"
+        end = f"1.{current_input_end_index}"
+        spell_check_status = partial_original == self.input_text.get()
+        if (self.spell_check_start == self.highlighted_start) & (
+            spell_check_status != self.current_word_spellcheck_status
+        ):
+            if spell_check_status:
+                self.original_textbox.tag_remove(
+                    "incorrect_input", self.spell_check_start, self.spell_check_end
+                )
+            else:
+                self.original_textbox.tag_remove(
+                    "correct_input", self.spell_check_start, self.spell_check_end
+                )
+        if spell_check_status:
+            self.original_textbox.tag_add("correct_input", start, end)
+        else:
+            self.original_textbox.tag_add("incorrect_input", start, end)
+        self.spell_check_start = start
+        self.spell_check_end = end
+        self.current_word_spellcheck_status = spell_check_status
+
+    def format_final_word(self):
+        if self.current_word_spellcheck_status:
+            self.original_textbox.tag_remove(
+                "correct_input", self.highlighted_start, self.highlighted_end
+            )
+            self.original_textbox.tag_remove(
+                "incorrect_input", self.highlighted_start, self.highlighted_end
+            )
+            self.original_textbox.tag_add(
+                "correct_word", self.highlighted_start, self.highlighted_end
+            )
+        else:
+            self.original_textbox.tag_remove(
+                "correct_input", self.highlighted_start, self.highlighted_end
+            )
+            self.original_textbox.tag_add(
+                "incorrect_input", self.highlighted_start, self.highlighted_end
+            )
+
     def key_handler(self, event):
-        if self.timer.running:
-            # TODO:
-            # if the input_text mismatches the current word they are meant to type
-            # font of word should be red, otherwise, black
+        if len(self.key_strokes) == 0:
+            print("starting timer")
+            self.start_test()
+        if self.timekeeper.timer.running:
+            self.highlight_current_word()
             self.key_strokes.append(event.char)
             if event.keysym == "space":
+                self.format_final_word()
+                self.current_word_index += 1
+                self.current_word_spellcheck_status = None
+                try:
+                    self.current_word = self.words_to_copy[self.current_word_index]
+                except IndexError:
+                    return
+                self.current_char_index = self.current_end_index + 1
                 self.get_word_from_input()
+            else:
+                self.spellcheck_current_input()
 
     def check_typing(self):
-        self.get_word_from_input()
         self.total_characters = len(self.key_strokes)
         self.total_words = len(self.final_input_words)
         self.correct_words = []
-        for example_word, input_word in zip(self.text_to_copy, self.final_input_words):
+        self.incorrect_words = []
+        for example_word, input_word in zip(self.words_to_copy, self.final_input_words):
             if example_word.lower().strip() == input_word.lower().strip():
                 self.correct_words.append(example_word.strip())
         self.total_correct_words = len(self.correct_words)
-        self.wpm = self.total_words / self.timer.original_time
-        self.kpm = self.total_characters / self.timer.original_time
-        self.cwpm = self.total_correct_words / self.timer.original_time
-        self.accuracy = self.total_correct_words / len(self.text_to_copy)
+
+        # TODO: update to logic outlined in https://typing-speed-test.aoeu.eu FAQ
+        self.kpm = self.total_characters / self.test_duration
+        self.cwpm = self.total_correct_words / self.test_duration
+        self.wpm = self.total_words / self.test_duration
+        self.accuracy = self.total_correct_words / len(
+            self.words_to_copy[: self.current_word_index]
+        )
         self.show_scores()
 
     def show_scores(self):
--- ui.py
+++ ui.py
@@ -2,7 +2,6 @@ import tkinter as tk
 
 import ttkbootstrap as ttk
 
-from timer import Timekeeper
 from typer import TypeTest
 
 
@@ -10,12 +9,7 @@ class UI:
     def __init__(self, root):
         self.window = root
         self.window.title("Typing Test")
-        self.test_texts = ["one word and another and another", "TEXT_B"]
-        self.current_text = 0
-        self.timekeeper = Timekeeper(self.window)
-        self.timekeeper.new_timer(6)
-        self.type_test = TypeTest(self.window, self.timekeeper.timer)
-        self.type_test.create_original_textbox()
+        self.type_test = TypeTest(self.window)
         self.create_input_widgets()
         self.add_buttons()
         self.welcome_screen()
@@ -49,41 +43,11 @@ class UI:
         if first_time:
             self.welcome.destroy()
         else:
-            self.current_text += 1
-            self.timekeeper.timer.time = 60
-            self.type_test.reset_vars(self.timekeeper.timer)
-        self.refresh_text()
-        self.start_timer()
-
-    def start_timer(self):
-        self.timekeeper.timer.start()
-        self.window.after(1000, self.timekeeper.tick)
-        self.type_test.input_text.config(state=tk.NORMAL)
-        self.restart_button.config(state=tk.DISABLED)
-        self.run_test()
-
-    def run_test(self):
-        if self.timekeeper.timer.running:
-            self.window.after(1000, self.run_test)
-        else:
-            self.type_test.input_text.config(state="disabled")
-            self.type_test.check_typing()
-            self.restart_button.config(state=tk.NORMAL)
+            self.type_test.reset_vars()
+        # self.restart_button.config(state=tk.DISABLED)
 
     def exit_game(self):
         self.window.destroy()
-
-    def refresh_text(self):
-        self.type_test.input_text.configure(state=tk.NORMAL)
-        self.type_test.input_text.delete(0, tk.END)
-        try:
-            text = self.test_texts[self.current_text]
-        except IndexError:
-            print("out of range")
-            text = self.test_texts[0]
-            self.current_text = 0
-        finally:
-            self.type_test.update_original_textbox(text)
 
     def welcome_screen(self):
         self.welcome = ttk.Toplevel(self.window)
